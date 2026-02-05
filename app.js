74c834aafd544f901880c09826f256a035e2b9e3
Icaro Angelo <angelopb1105@gmail.com>
2026-02-05 20:11:53 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./src/swagger/swagger.json');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+const productRoutes = require('./src/routes/productRoutes');
+const categoryRoutes = require('./src/routes/categoryRoutes');
+
+app.use('/products', productRoutes);
+app.use('/categories', categoryRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
---
+++ src/controllers/categoryController.js
@@ -0,0 +1,29 @@
+const Category = require('../models/category');
+
+exports.getAllCategories = async (req, res) => {
+  const categories = await Category.find();
+  res.json(categories);
+};
+
+exports.getCategoryById = async (req, res) => {
+  const category = await Category.findById(req.params.id);
+  if (!category) return res.status(404).json({ message: 'Category not found' });
+  res.json(category);
+};
+
+exports.createCategory = async (req, res) => {
+  const category = new Category(req.body);
+  await category.save();
+  res.status(201).json(category);
+};
+
+exports.updateCategory = async (req, res) => {
+  const category = await Category.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  if (!category) return res.status(404).json({ message: 'Category not found' });
+  res.json(category);
+};
+
+exports.deleteCategory = async (req, res) => {
+  const category = await Category.findByIdAndDelete(req.params.id);
+  if (!category) return res.status(404).json({ message: 'Deleted successfully' });
+};
