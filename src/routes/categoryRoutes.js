763017f98ba027de51294c493b8539f312f09513
Icaro Angelo <angelopb1105@gmail.com>
2026-02-11 20:55:22 -0300

    fixed category routes

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const isAuth = require('../middlewares/isAuth');
 const controller = require('../controllers/categoryController');
 
 router.get('/', controller.getAllCategories);
