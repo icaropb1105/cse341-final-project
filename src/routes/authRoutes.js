812f89a758df0f2328c6015c0eec88dd2f3e36a4
Icaro Angelo <angelopb1105@gmail.com>
2026-02-10 22:04:42 -0300

    debugging 2

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,20 +1,23 @@
-import express from 'express';
-import passport from 'passport';
+const express = require('express');
+const passport = require('passport');
 
 const router = express.Router();
 
-// Google OAuth
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-router.get(
-  '/google/callback',
-  passport.authenticate('google', { session: false, failureRedirect: '/' }),
+router.get('/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+
+router.get('/google/callback',
+  passport.authenticate('google', { session: false, failureRedirect: '/login' }),
   (req, res) => {
+   
     res.json({
-      message: 'Login successful',
+      message: 'Login com Google OK',
       user: req.user
     });
   }
 );
 
-export default router;
+module.exports = router;
