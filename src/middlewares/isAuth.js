2715f2bb17824cdf76ed87ff917d7863dcb80117
Icaro Angelo <angelopb1105@gmail.com>
2026-02-11 19:59:59 -0300

    fixing auth 2

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -7,13 +7,23 @@ module.exports = (req, res, next) => {
     return res.status(401).json({ message: 'No token provided' });
   }
 
-  const token = authHeader.split(' ')[1];
+  const parts = authHeader.split(' ');
+
+  if (parts.length !== 2) {
+    return res.status(401).json({ message: 'Token error' });
+  }
+
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).json({ message: 'Token malformatted' });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
-    next();
+    return next();
   } catch (err) {
-    return res.status(401).json({ message: 'Invalid token' });
+    return res.status(401).json({ message: 'Token invalid' });
   }
 };
