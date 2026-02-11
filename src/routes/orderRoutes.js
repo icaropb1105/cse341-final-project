04faae038db3ed1979b95ef0dfa5a6ba2a594154
Icaro Angelo <angelopb1105@gmail.com>
2026-02-10 21:38:12 -0300

    fixed middlewares

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const isAuth = require('../middleware/isAuth');
+const isAuth = require('../middlewares/isAuth');
 const { body, validationResult } = require('express-validator');
 const {
   getAllOrders,
